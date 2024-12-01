ac6949f80dd02698f16b3a3e155438abf6cc16d5
Devin Chaney <135728867+shout64@users.noreply.github.com>
2024-12-01 00:24:35 -0600

    Day 1 First Star

--- DayOne/dayone.py
+++ DayOne/dayone.py
@@ -2,24 +2,27 @@ import lists
 
 def compare_lists(leftlist, rightlist):
     
-    leftlow = 10
-    rightlow = 10
     totaldistance = 0
 
+    while len(leftlist) > 0:
+        leftlow = 100000
+        rightlow = 100000
 
-    for i in leftlist:
-        if i < leftlow:
-            leftlow = i
-    leftlist.remove(leftlow)
+        for i in leftlist:
+            if i < leftlow:
+                leftlow = i
+        
+        leftlist.remove(leftlow)
 
-    for i in rightlist:
-        if i < rightlow:
-            rightlow = i
-    rightlist.remove(rightlow)
+        for i in rightlist:
+            if i < rightlow:
+                rightlow = i
+        
+        rightlist.remove(rightlow)
 
-    distance = abs(leftlow - rightlow)
+        distance = abs(leftlow - rightlow)
 
-    totaldistance += distance
+        totaldistance += distance
 
     print(f"Total distance is: {totaldistance}")
 
