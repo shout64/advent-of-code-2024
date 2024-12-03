6731942bbc36a6d24a50c837bceffc5f0f33ddfc
shout64 <135728867+shout64@users.noreply.github.com>
2024-12-03 16:16:06 -0600

    Update to loop through function

--- DayTwo/daytwo.py
+++ DayTwo/daytwo.py
@@ -12,36 +12,38 @@ report_list = [l.split(" ") for l in data.split("\n") if l]
 
 def analyze_reports(reports):
     num_of_safe_reports = 0
+    last_number = 0
+    report_safe = True
+    increasing = False
+    decreasing = False
+    index = 0
 
-    for i in report_list:
-        last_number = 0
-        report_safe = True
-        increasing = False
-        decreasing = False
-        index = 0
+    for j in reports:
+        if (index != 0):
+            if int(j) < last_number:
+                decreasing = True
+                # print("Decreasing: ", decreasing)
+            elif int(j) > last_number:
+                increasing = True
+                # print("Increasing: ", decreasing)
 
-        for j in i:
-            if (index != 0):
-                if int(j) < last_number:
-                    decreasing = True
-                    # print("Decreasing: ", decreasing)
-                elif int(j) > last_number:
-                    increasing = True
-                    # print("Increasing: ", decreasing)
+        if increasing == True and decreasing == True:
+            report_safe = False
 
-            if increasing == True and decreasing == True:
-                report_safe = False
+        difference = abs(int(j) - last_number)
+        if (index != 0) and (difference not in [1, 2, 3]):
+            report_safe = False
 
-            difference = abs(int(j) - last_number)
-            if (index != 0) and (difference not in [1, 2, 3]):
-                report_safe = False
+        last_number = int(j)
+        index += 1
 
-            last_number = int(j)
-            index += 1
+    if report_safe == True:
+        num_of_safe_reports += 1
 
-        if report_safe == True:
-            num_of_safe_reports += 1
+    return num_of_safe_reports
 
-    print(f"Number of safe reports: {num_of_safe_reports}")
+for i in range(len(report_list)):
+    safies = analyze_reports(report_list[i])
 
-analyze_reports(report_list)
+    print(safies)
+
