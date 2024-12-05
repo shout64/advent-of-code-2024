1a2c4f8a38c0014982cd1973772a5c2fff8958bd
shout64 <135728867+shout64@users.noreply.github.com>
2024-12-05 16:37:45 -0600

    Update daythree.py

--- day-03/daythree.py
+++ day-03/daythree.py
@@ -1,12 +1,45 @@
+# Open file
 input = open("input.txt")
 data = input.read()
 
 result = 0
 
-start = data.find("mul(")
-end = (data[1:].find(")")) + 2
+def find_muls(data):
+    all_muls = []
+    numbers = []
+    string_num = ""
+    start_index = 0
+    more_muls = True
 
-print(data[start:end])
+    start = data.find("mul(", start_index)
+    sample_string = data[start:]
+    for char in sample_string:
+        if char.isdigit():
+            string_num += char
+        elif char == ")":
+            break #Prolly not right
+        else:
+            if string_num != "":
+                numbers.append(int(string_num))
+                string_num = ""
 
-# TODO: Find how to pull numbers out of string, maybe using "re" library.
+    #Probably check for invalid characters or start next loop
+    all_muls.append(sample_string)
+    start_index = end
+
+    print(all_muls)
+    
+find_muls(data)
+
+numbers = []
+string_num = ""
+
 # TODO: Write function to look through data, pull each mul and add to result. Break if contains invalid characters.
+
+# for char in sample_string:
+#     if char.isdigit():
+#         string_num += char
+#     else:
+#         if string_num != "":
+#             numbers.append(int(string_num))
+#             string_num = ""
