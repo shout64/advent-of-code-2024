276ec8fc6366ae4739e555ec1760a787fd5f7966
shout64 <135728867+shout64@users.noreply.github.com>
2024-12-05 19:16:12 -0600

    Almost functioning part 1

--- day-03/daythree.py
+++ day-03/daythree.py
@@ -2,44 +2,46 @@
 input = open("input.txt")
 data = input.read()
 
-result = 0
 
 def find_muls(data):
-    all_muls = []
+    result = 0
     numbers = []
     string_num = ""
     start_index = 0
     more_muls = True
 
-    start = data.find("mul(", start_index)
-    sample_string = data[start:]
-    for char in sample_string:
-        if char.isdigit():
-            string_num += char
-        elif char == ")":
-            break #Prolly not right
-        else:
-            if string_num != "":
+    while more_muls == True:
+        start = data.find("mul(", start_index)
+        print(f"Start: {start}")
+        sample_string = data[start:]
+        print(f"String: {sample_string}")
+        if start == -1:
+            more_muls = False
+            print(f"Result: {result}")
+            break
+        for char in sample_string:
+            if char.isdigit():
+                string_num += char
+                print("Found digit: ", string_num)
+            elif char == ",":
                 numbers.append(int(string_num))
+                print("Found full number: ", string_num)
                 string_num = ""
-
-    #Probably check for invalid characters or start next loop
-    all_muls.append(sample_string)
-    start_index = end
-
-    print(all_muls)
+            elif char == ")":
+                numbers.append(int(string_num))
+                if len(numbers) == 2:
+                    mult_number = numbers[0] * numbers[1]
+                    print(f"Success! {mult_number} added to result.")
+                    result += mult_number
+                    print(f"Running result: {result}\n")
+                    start_index = start + 4
+                    numbers = []
+                    break
+            else:
+                print(f"NOT MUL VALUE {char}")
+                string_num = ""
+                start_index = start + 4
+                numbers = []
+        
     
 find_muls(data)
-
-numbers = []
-string_num = ""
-
-# TODO: Write function to look through data, pull each mul and add to result. Break if contains invalid characters.
-
-# for char in sample_string:
-#     if char.isdigit():
-#         string_num += char
-#     else:
-#         if string_num != "":
-#             numbers.append(int(string_num))
-#             string_num = ""
